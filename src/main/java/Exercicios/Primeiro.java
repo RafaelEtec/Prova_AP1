f585acff77ee25660749e153d43e509e5d59fcec
Rafael.fgoulart1 <Rafael.fgoulart1@CAS560823W1016.senacsp.edu.br>
2022-05-02 19:43:11 -0300

    Em andamento

---
+++ src/main/java/Exercicios/Primeiro.java
@@ -0,0 +1,57 @@
+package Exercicios;
+import java.util.Scanner;
+/** 
+    por Rafael Ferreira Goulart
+   
+    Implementar uma função para calcular o total de uma venda de açaí a partir das 
+    quantidades compradas para cada tamanho: pequeno, médio e grande, sabendo 
+    que o valor do açaí é R$ 13,50, R$ 15,00 e R$ 17,50 respectivamente.
+**/
+public class Primeiro {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        int qtdPequeno, qtdMedio, qtdGrande;
+        String strNumNegativo = 
+                "O número não pode ser negativo!"
+              + "Insira novamente: ";
+        
+        System.out.println("Quantos Açaís pequenos?: ");
+        qtdPequeno = ent.nextInt();
+        while (qtdPequeno < 0) {
+            System.out.println(strNumNegativo);
+            qtdPequeno = ent.nextInt();
+        }
+        System.out.println("Quantos Açaís médios?: ");
+        qtdMedio = ent.nextInt();
+        while (qtdMedio < 0) {
+            System.out.println(strNumNegativo);
+            qtdMedio = ent.nextInt();
+        }
+        System.out.println("Quantos Açaís grandes?: ");
+        qtdGrande = ent.nextInt();
+        while (qtdGrande < 0) {
+            System.out.println(strNumNegativo);
+            qtdGrande = ent.nextInt();
+        }
+        
+        double precoTotal = calcularTotal(qtdPequeno, qtdMedio, qtdGrande);
+        System.out.println(
+                "Você comprou: \n"
+              + qtdPequeno+" - Pequeno \n"
+              + qtdMedio+" - Médio \n"
+              + qtdGrande+" - Grande \n"
+              + "Preço a pagar: R$"+precoTotal);
+    }
+    
+    public static double calcularTotal(int qtdPequeno, int qtdMedio, int qtdGrande) {
+        double precoTotal, totalPequeno, totalMedio, totalGrande;
+        
+        totalPequeno = qtdPequeno * 13.50;
+        totalMedio = qtdMedio * 15.00;
+        totalGrande = qtdGrande * 17.50;
+        
+        precoTotal = totalPequeno + totalMedio + totalGrande;
+        
+        return precoTotal;
+    }
+}
---
+++ src/main/java/Exercicios/Segundo.java
@@ -0,0 +1,42 @@
+package Exercicios;
+import java.util.Scanner;
+/** 
+    por Rafael Ferreira Goulart
+   
+    Implementar uma função que calcule o total a se pagar em uma compra na sorveteria, 
+    a partir do total de gramas comprado. O preço de 100g é R$ 3,50 mas se o total de 
+    gramas for maior que 1kg, o preço de 100g diminui 50 centavos.
+**/
+public class Segundo {
+    public static void main(String[] args) {
+        Scanner ent = new Scanner(System.in);
+        double gramas;
+        
+        System.out.println("Quantas gramas de sorvete?: ");
+        gramas = ent.nextDouble();
+        while (gramas < 0) {
+            System.out.println(
+                "O número não pode ser negativo!"
+              + "Insira novamente: ");
+            gramas = ent.nextDouble();
+        }
+        
+        double precoTotal = calcularTotal(gramas);
+        System.out.println(
+                "Você Comprou: \n"
+              + gramas+"g de sorvete \n"
+              + "Preço a pagar: R$"+precoTotal);
+    }
+    
+    public static double calcularTotal(double gramas) {
+        double precoTotal;
+        
+        if (gramas >= 1000) {
+            precoTotal = gramas * 3.50;
+        } else {
+            precoTotal = gramas * 3.00;
+        }
+        
+        return precoTotal;
+    }
+}
